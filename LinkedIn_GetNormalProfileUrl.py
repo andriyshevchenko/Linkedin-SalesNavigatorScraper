82016363329e10262f4e8f9b9fea1f9df89ced49
Andrii Shevchenko <globallogic_andriis@nice.com>
2023-11-27 11:27:44 +0000

    Close connection

--- LinkedIn_GetNormalProfileUrl.py
+++ LinkedIn_GetNormalProfileUrl.py
@@ -114,7 +114,10 @@ async def scrap_from_sql(log, limit: int):
             print('Waiting...')
             time.sleep(45)   
 
+        impacted_profiles_number = remaining_profiles_number - int(await connection.fetchval('SELECT COUNT(*) FROM current_working_copy'))
+        await log.write(f'Processed profiles: {impacted_profiles_number}')
         driver.quit()
+        connection.close()
 
 def constructDriver(headless = False):
     options = Options()
