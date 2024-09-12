32c81074a5d5df76d30377aeddd8ea7ca9e545a7
Andrii Shevchenko <shewchenkoandriy@gmail.com>
2024-09-12 16:41:35 +0300

    Updated logging and driver creation for disconnect-leads function

--- LinkedIn_DisConnectLeads.py
+++ LinkedIn_DisConnectLeads.py
@@ -1,3 +1,7 @@
+import logging
+import os
+import platform
+import stat
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
@@ -10,6 +14,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.core.driver_cache import DriverCacheManager
 import pathlib 
 import random
+from ErrorLog import ErrorLog
+from LogByLevel import LogByLevel
 from TelegramLog import TelegramLog, NullLog
 from telegram import Bot
 import asyncio
@@ -36,48 +42,123 @@ async def withdraw_connections(driver, logger):
         time.sleep(random.uniform(5.0, 10.0))
     await logger.write(f'Withdrawn {len(buttons)} requests')
 
-def constructDriver(headless = False):
+async def constructDriver(log):
+    await log.write('Creating a driver', logging.DEBUG)
+
     options = Options()
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument('--no-sandbox')
-
-    if headless:
-        options.add_argument("--window-size=1280,700")
-        options.add_argument("--headless=new")
-        options.add_experimental_option(
-            "prefs", {"profile.managed_default_content_settings.images": 2})
     options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36')
-    cache_manager = DriverCacheManager(root_dir=pathlib.Path.cwd())
+    options.add_argument("--headless=new")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_experimental_option(
+            "prefs", {"profile.managed_default_content_settings.images": 2})
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager(cache_manager=cache_manager).install(), options=options))
+    driver: any
 
-    driver.get('https://www.linkedin.com/sales')
-    #driver.maximize_window()
-    username = WebDriverWait(driver=driver, timeout=60).until(
-        EC.presence_of_element_located((By.ID,'username'))
-    )
-    username.send_keys('shewchenkoandriy@gmail.com')  # Insert your e-mai
-    password = WebDriverWait(driver=driver, timeout=60).until(
-        EC.presence_of_element_located((By.ID,'password'))
-    )
-    password.send_keys('BinanceZalupa228')  # Insert your password her
-    log_in_button = WebDriverWait(driver=driver, timeout=60).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[class="btn__primary--large from__button--floating"')))
+    if platform.system() == "Windows":
+        await log.write('Running on Windows', logging.INFO)
+
+        options.add_argument("--window-size=1280,700")
+        
+        cache_manager = DriverCacheManager(root_dir=pathlib.Path.cwd())
+        chrome_install = ChromeDriverManager(cache_manager=cache_manager).install()
+
+        folder = os.path.dirname(chrome_install)
+        chromedriver_path = os.path.join(folder, "chromedriver.exe")
+
+        driver = webdriver.Chrome(service=Service(executable_path=chromedriver_path, options=options))
+    else:
+        await log.write('Running on Linux', logging.INFO)
+
+        if 'DOCKER' in os.environ:
+            await log.write('Running from Docker', logging.INFO)
+
+        options.add_argument("start-maximized")
+        options.binary_location = '/usr/bin/google-chrome'
+
+        path = ChromeDriverManager().install().replace('THIRD_PARTY_NOTICES.', '')
+        
+        await log.write(f'Installed chrome web driver to: {path}', logging.DEBUG)
+        
+        await log.write(f'Setting permission', logging.DEBUG)
+        
+        # Set the file to be executable by the owner, group, and others
+        os.chmod(path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |  # Owner
+                       stat.S_IRGRP | stat.S_IXGRP |               # Group
+                       stat.S_IROTH | stat.S_IXOTH)                # Others
+
+        service = Service(path)
+
+        await log.write('Starting Chrome', logging.DEBUG)
+
+        driver = webdriver.Chrome(service=service, options=options)
     
-    time.sleep(random.uniform(5.0, 10.0))
-    log_in_button.click()
-    time.sleep(60)
-    driver.switch_to.window(driver.current_window_handle)
-    return driver
+    await log.write('Driver installed', logging.DEBUG)
+
+    attempts = 0
+    while attempts < 3:
+        try:
+            await log.write('Logging user in', logging.INFO)
+
+            attempts += 1
+            if attempts == 1:
+                driver.get('https://www.linkedin.com/sales')
+            else:
+                driver.refresh()
+
+            driver.maximize_window()
+
+            await log.write(f'Waiting berween 5 and 10 seconds', logging.DEBUG)
+
+            time.sleep(random.uniform(5.0, 10.0))
+            
+            await log.write('Typing email and password', logging.DEBUG)
+            
+            username = WebDriverWait(driver=driver, timeout=60).until(
+                EC.presence_of_element_located((By.ID,'username'))
+            )
+            username.send_keys('shewchenkoandriy@gmail.com')  # Insert your e-mai
+            password = WebDriverWait(driver=driver, timeout=60).until(
+                EC.presence_of_element_located((By.ID,'password'))
+            )
+            password.send_keys('BinanceZalupa228')  # Insert your password her
+            log_in_button = WebDriverWait(driver=driver, timeout=60).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[class="btn__primary--large from__button--floating"')))
+
+            await log.write(f'Waiting berween 5 and 10 seconds', logging.DEBUG)
+
+            time.sleep(random.uniform(5.0, 10.0))
+
+            await log.write('Pressing login button', logging.DEBUG)
+
+            log_in_button.click()
+
+            await log.write(f'Waiting 60 seconds', logging.DEBUG)
+
+            time.sleep(60)
+            driver.switch_to.window(driver.current_window_handle)
+            return driver
+        except asyncio.CancelledError:
+            await log.write('Job was cancelled', logging.WARNING)
+            driver.quit()
+            break
+        except Exception as error:
+            await log.write(f'Login error: \n\n{ErrorLog(error)}', logging.ERROR)
+            if attempts == 3:
+                driver.quit()
+                raise error
 
 async def main():
-    log = NullLog(Bot(token='6464053578:AAGbooTDuVCdiYqMhN2akhMMEJI0wVZSr7k'), '-1002098033156', 'DisconnectLeads')  
-    await log.write('Function started')
-    driver = constructDriver(True)
-    await log.write('Successfully started scraper')
+    log = LogByLevel(TelegramLog(Bot(token='7209921522:AAHRhEH11Clg_qBPY9SSwfEJDoPvJ5yso70'), '-1002300475780', 'DisonnectLeads'), logging.DEBUG)  
+    driver = await constructDriver(log)
+    await log.write('Successfully started function', logging.DEBUG)
     driver.get('https://www.linkedin.com/mynetwork/invitation-manager/sent/')
     time.sleep(random.uniform(5.0, 10.0))
     perform_scroll_to_bottom(driver)
@@ -91,15 +172,15 @@ async def main():
         pages = list(map(lambda number: f'https://www.linkedin.com/mynetwork/invitation-manager/sent?page={number}', range(1, number_pages+1)))
     except:
         pass
-    await log.write(f'There are {number_pages} pages. Withdrawing requests...')
+    await log.write(f'There are {number_pages} pages. Withdrawing requests...', logging.DEBUG)
     for page in pages:
         driver.get(page)
         time.sleep(random.uniform(5.0, 10.0))
         try:
             await withdraw_connections(driver, log)
         except Exception as error:
-            await log.write(f'Problem encontered: {error}')
-    await log.write('Function quit')
+            await log.write(f'Problem encontered: {error}', logging.DEBUG)
+    await log.write('Function quit', logging.DEBUG)
 
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
